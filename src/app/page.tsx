88403a94919f0f357902b762fe392839c6e9e04f
Ismail <hummetzadeismail@gmail.com>
2023-11-10 23:43:51 +0300

    set header on layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { Header } from '@/widgets/header';
 import Image from 'next/image';
 import style from './style.module.scss';
 
 export default function Home() {
-	return (
-		<div>
-			<Header />
-		</div>
-	);
+	return <div></div>;
 }
